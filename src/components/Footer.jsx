178e16cf02a7f10dafc10ec14f653f60380d2473
Aleks <aleks.mihelcic@gmail.com>
2022-03-10 22:46:17 +0100

    Google ads

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,10 @@
 function Footer() {
     return (
         <div className="bg-transparent flex gap-x-10 gap-y-1 flex-col md:flex-row w-[100%] text-center text-sm py-2 justify-center">
-            <a className="underline" href="support.html" target="_blank" >Support</a>
+            <a className="underline" href="support.html?utm_source=galactic-bot.com&utm_medium=footer" target="_blank" >Support</a>
             <a className="underline" href="terms-of-service.html" target="_blank" >Terms Of Service</a>
             <a className="underline" href="privacy-policy.html" target="_blank" >Privacy Policy</a>
             Galactic @2022
-            <img src="https://top.gg/api/widget/status/580497645882048532.svg?noavatar=true" className="w-max m-auto md:m-0" alt="Bot status" />
-
         </div>
     );
 }
